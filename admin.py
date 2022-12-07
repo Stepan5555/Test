e4738b05b32ce6c888d240b08ecd2b51bda88627
Stepan <stepan.manjikyan@realschool.am>
2022-12-07 09:26:58 +0400

    first commit

---
+++ admin.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+
+from . models import Women
+from . models import Category
+# from . models import FlightMissions
+
+
+
+class WomenMen(admin .ModelAdmin):
+      WomenMen = ["content"]
+
+
+
+admin.site.register(Women)
+admin.site.register(Category)
+# admin.site.register(FlightMissions)
